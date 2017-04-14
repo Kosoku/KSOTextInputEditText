5d6a7a4ad16dd82421ebec105f00693ba952fa63
Jason R. Anderson <jason@kosoku.com>
2017-04-13 20:55:11 -0400

    set up project and workspace

---
+++ KSOTextInputEditText/KSOTextInputEditText.h
@@ -0,0 +1,19 @@
+//
+//  KSOTextInputEditText.h
+//  KSOTextInputEditText
+//
+//  Created by Jason Anderson on 4/13/17.
+//  Copyright © 2017 Kosoku Interactive, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KSOTextInputEditText.
+FOUNDATION_EXPORT double KSOTextInputEditTextVersionNumber;
+
+//! Project version string for KSOTextInputEditText.
+FOUNDATION_EXPORT const unsigned char KSOTextInputEditTextVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KSOTextInputEditText/PublicHeader.h>
+
+
